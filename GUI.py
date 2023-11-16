182ec877c28fcf6cbf38f3282929ac74dee99006
lecrowpus <95230960+lecrowpus@users.noreply.github.com>
2023-11-16 17:17:31 +0530

    Add files via upload

--- GUI.py
+++ GUI.py
@@ -7,11 +7,13 @@ import main
 import threading
 import json
 from tkinter import messagebox
+import backend
+
 
 class App(customtkinter.CTk):
     def __init__(self):
         #all configuration
-        self.bk=main.backend_process()
+        
         super().__init__()
         customtkinter.set_appearance_mode("system")  
         ctk.set_default_color_theme("dark-blue")
@@ -20,28 +22,36 @@ class App(customtkinter.CTk):
         self.grid_columnconfigure(1, weight=1)
         self.grid_rowconfigure(0, weight=1)
         
-
+        
         # defining variables
+        self.bk=main.backend_process(CWP="default")
+        self.Direct_Backend=backend.backendworks(CWP="default")
+        self.removeModules_FromCreatedProj_list=[]
+        self.installModuleToProject=[]
         self.moduleCheckBoxList_name=[]#this list contains the checkbox that are created in for loop
-        self.moduleversionList_version=[]
+        self.moduleVersionList=[]
         self.Mdnvrlist=[]
-        self.moduledata=self.bk.getModules_dict()
+        self.moduledata=self.Direct_Backend.getModules_dict()
         self.ddir="None"
+        self.Rem_module_data=[]
         
         #crating main frames
-        self.nav=ctk.CTkFrame(self,bg="blue")
+        self.nav=ctk.CTkFrame(self)
         self.nav.grid(sticky="ns",padx=10,pady=10)
 
         self.startbtn=ctk.CTkButton(self.nav,text="➕ New project",fg_color="#0286fa",command=self.Frame_createprojframe)
         self.startbtn.grid(column=0,padx=10,pady=10)
+
+        # displaying projects
         with open('data.json', 'r') as file:
             ProjectData = json.load(file)
+
         for project in ProjectData["Project"]:
             viewProvectbtn=ctk.CTkButton(self.nav,text=project,command=lambda i=project: self.viewProjectFunc(projectName=i))
             viewProvectbtn.grid(column=0,padx=10,pady=10)
         
-
-        self.workspaceFrame=ctk.CTkFrame(self,bg="blue")
+        # creating widgets
+        self.workspaceFrame=ctk.CTkFrame(self)
         self.workspaceFrame.grid(row=0,column=1,sticky="nsew",pady=10)
         self.workspaceFrame.grid_columnconfigure(1, weight=200)
         self.workspaceFrame.grid_rowconfigure(0,weight=200)
@@ -55,7 +65,7 @@ class App(customtkinter.CTk):
 
         """all the widgets for the create python project """
         self.ChooseDir=ctk.CTkButton(self.python_projsettings,text="Choose folder",command=self.choosedir)
-        
+        self.CancleCreatingProject=ctk.CTkButton(self.python_projsettings,text="Cancle",fg_color="#a83232",width=50,command=self.Frame_remprojframe)
         self.path_lable=ctk.CTkLabel(self.python_projsettings,text="placeholder/path/to/project")
         
         self.rdcheck=IntVar()
@@ -77,7 +87,6 @@ class App(customtkinter.CTk):
         
         self.VersionWariningLable=ctk.CTkLabel(self.python_projsettings,text="invalid version ",text_color="red")
 
-
         self.versionVar=StringVar()
         self.versionentry=ctk.CTkEntry(self.python_projsettings,textvariable=self.versionVar)
 
@@ -86,32 +95,42 @@ class App(customtkinter.CTk):
         self.moduleVar=StringVar()
         self.moduleEntey=ctk.CTkEntry(self.python_projsettings,textvariable=self.moduleVar)
 
-        self.modulenameframe=ctk.CTkFrame(self.python_projsettings)
-        self.moduleversionframe=ctk.CTkFrame(self.python_projsettings)
+        self.modulenameframe=ctk.CTkScrollableFrame(self.python_projsettings)
+        # self.moduleversionframe=ctk.CTkScrollableFrame(self.python_projsettings)
         
+        self.addmodulesentryes(moduledata_=self.moduledata)
         
-        # self.addmodulesentryes(moduledata_=self.moduledata)
         
         """all the wordgets for Viewing creted project """
 
-        #display the modules installed
-        #Git
+        self.saveChangeBtn=ctk.CTkButton(self.python_projsettings,text="Save",fg_color="#32a852",width=50 ,command=self.saveProject)
     
     def viewProjectFunc(self,projectName):
-        with open('data.json', 'r') as file:
-            ProjectData = json.load(file)
-        dir=str(ProjectData["Project"][projectName]["settings"]).replace("/", "\\")
-        self.modulenameframe.grid(row=6,column=1,sticky="nwe",padx=10,columnspan=3)
-        self.moduleversionframe.grid(row=6,column=0,sticky="nwe",padx=10)
-        self.moduledata=self.bk.getmodules_json(dir=str(dir))
-        print(self.moduledata)
-        self.addmodulesentryes(moduledata_=self.moduledata)
-    def addmodulesentryes(self,moduledata_):
+        self.Direct_Backend.changeproject(projectName)
         
+        self.addMbtn.grid(column=2,row=5,padx=10,pady=30,sticky="n")
+        self.remMbtn.grid(column=3,row=5,padx=10,pady=30,sticky="n")
+        self.CancleCreatingProject.grid(row=0,column=3,padx=10,pady=10)
+        self.versionentry.grid(row=5,column=0,sticky="we",padx=10,pady=10)
+        self.moduleEntey.grid(row=5,column=1,sticky="we",padx=10,pady=10)
+        self.modulenameframe.grid(row=6,column=0,sticky="nwe",padx=10,columnspan=3)
+        self.moduledata=self.Direct_Backend.getmodules_json()
+        self.saveChangeBtn.grid(row=0,column=2,padx=10,pady=10)
+        self.CancleCreatingProject.configure(command=self.Frame_remViewprojframe)
+
+        self.addmodulesentryes(moduledata_=self.moduledata)
+        for Cb in self.moduleCheckBoxList_name:
+            Cb.grid(padx=10,pady=10)
+        for Cb in self.moduleVersionList_version:
+            Cb.grid(padx=10,pady=10)
+
+        self.startbtn.configure(state="disabled")
+
+    def addmodulesentryes(self,moduledata_):
+        #refreashing the lists for use
         self.moduleCheckBoxList_name=[]
         self.moduleVersionList_version=[]
         self.Mdnvrlist=[]
-
         self.Mdnlist=[]
     
         for item in moduledata_["modules"]:
@@ -120,14 +139,18 @@ class App(customtkinter.CTk):
             mdlNamecheckB=ctk.CTkCheckBox(self.modulenameframe,text=item,variable=varMdn,onvalue=item,offvalue="off")
             self.moduleCheckBoxList_name.append(mdlNamecheckB)
             
-            mdlVersioncheckB=ctk.CTkLabel(self.moduleversionframe,text=self.moduledata["modules"][item])
+            mdlVersioncheckB=ctk.CTkLabel(self.modulenameframe,text=self.moduledata["modules"][item])
             self.moduleCheckBoxList_name.append(mdlVersioncheckB)
+    
     def blank(self,*args):
         print(args)
         pass
+    
     def Frame_createprojframe(self):
         self.ChooseDir.grid(padx=10,pady=10)
         self.path_lable.grid(column=1,row=0,sticky="w",padx=10,columnspan=10)
+        self.CancleCreatingProject.grid(row=0,column=3,padx=10,pady=10)
+
         self.readmecheck.grid(row=1,column=0,sticky="nw",padx=10,pady=30)
         self.staticcheck.grid(row=2,column=0,sticky="nw",padx=10)
         self.ReqTxtCheck.grid(row=1,column=1,sticky="nw",padx=10,pady=30)
@@ -139,19 +162,28 @@ class App(customtkinter.CTk):
         self.versionentry.grid(row=5,column=0,sticky="we",padx=10,pady=10)
         self.moduleEntey.grid(row=5,column=1,sticky="we",padx=10,pady=10)
         
-        self.modulenameframe.grid(row=6,column=1,sticky="nswe",padx=10,columnspan=3)
-        self.moduleversionframe.grid(row=6,column=0,sticky="nswe",padx=10)
+        self.modulenameframe.grid(row=6,column=0,sticky="nswe",padx=10,columnspan=3)
         for Cb in self.moduleCheckBoxList_name:
-            Cb.grid(padx=10,pady=10)
+            Cb.grid(column=1,padx=10,pady=10)
         for Cb in self.moduleVersionList_version:
-            Cb.grid(padx=10,pady=10)
+            Cb.grid(column=0,padx=10,pady=10)
         self.startbtn.configure(state="disabled")
+    
     def Frame_remprojframe(self):
         self.python_projsettings.grid_forget()
         for Cb in self.moduleCheckBoxList_name:
             Cb.grid_forget()
         for Cb in self.moduleVersionList_version:
             Cb.grid_forget() 
+        self.startbtn.configure(state="normal")
+
+    def Frame_remViewprojframe(self):
+        self.python_projsettings.grid_forget()
+        for Cb in self.moduleCheckBoxList_name:
+            Cb.grid_forget()
+        for Cb in self.moduleVersionList_version:
+            Cb.grid_forget()
+        self.saveChangeBtn.grid_forget() 
         self.startbtn.configure(state="normal")
         
     def loop(self):
@@ -161,16 +193,15 @@ class App(customtkinter.CTk):
         self.ddir=filedialog.askdirectory()
         self.path_lable.configure(text=self.ddir)
         
-    def addmodule(self):
-        if self.bk.getModule(self.moduleVar.get(),self.versionVar.get())=="no such module":
+    def addmodule(self):#adds module to be installed 
+        if self.Direct_Backend.getModule(self.moduleVar.get(),self.versionVar.get())=="no such module":
             self.ModuleWariningLable.grid(row=4,column=1,sticky="s")
             
             
-        elif self.bk.getModule(self.moduleVar.get(),self.versionVar.get())=="no such version":
+        elif self.Direct_Backend.getModule(self.moduleVar.get(),self.versionVar.get())=="no such version":
             self.VersionWariningLable.grid(row=4,column=0,sticky="s")
             
         else:
-            
             self.ModuleWariningLable.grid_forget()
             self.VersionWariningLable.grid_forget()
             for Cb in self.moduleCheckBoxList_name:
@@ -180,39 +211,39 @@ class App(customtkinter.CTk):
             
             # tempdict={self.moduleVar.get():self.versionVar.get()}
             self.moduledata["modules"][self.moduleVar.get()] = str(self.versionVar.get())
-            
-            self.addmodulesentryes()
+            self.installModuleToProject[self.moduleVar.get()] = str(self.versionVar.get())
+            self.addmodulesentryes(moduledata_=self.moduledata)
             for Cb in self.moduleCheckBoxList_name:
-                Cb.grid(padx=10,pady=10)
+                Cb.grid(column=1,padx=10,pady=10)
             for Cb in self.moduleVersionList_version:
-                Cb.grid(padx=10,pady=10)
-
-            
-    def remmodule(self):
+                Cb.grid(column=0,padx=10,pady=10)
+   
+    def remmodule(self):#removes modules to be deleted or to me not installed
         for Cb in self.moduleCheckBoxList_name:
             Cb.grid_forget()
         for Cb in self.moduleVersionList_version:
             Cb.grid_forget()
         
+        self.removeModules_FromCreatedProj_list=[]
         
-
-        # Step 2: Remove the item from the dictionary
         i=0
         for var_ in self.Mdnlist:
             if var_.get() != "off":
                 if var_.get() in self.moduledata["modules"]:
+                    self.removeModules_FromCreatedProj_list.append(var_.get())
                     del self.moduledata["modules"][var_.get()]
                     var_.set(0)
                     self.Mdnlist.remove(var_)
                     self.moduleCheckBoxList_name.pop(i)
 
             i=i+1
-        self.addmodulesentryes()
+        self.addmodulesentryes(moduledata_=self.moduledata)
         for Cb in self.moduleCheckBoxList_name:
-            Cb.grid(padx=10,pady=10)
+            Cb.grid(column=1,padx=10,pady=10)
         for Cb in self.moduleVersionList_version:
-            Cb.grid(padx=10,pady=10)
+            Cb.grid(column=0,padx=10,pady=10)
         pass    
+        # self.Direct_Backend.Uninstall_Modules( Mdl=removeModules_FromCreatedProj_list)
     def createproject(self):
         if self.ddir=="None":
             messagebox.showwarning("Warning", "Choose the directory!")
@@ -220,6 +251,14 @@ class App(customtkinter.CTk):
         self.bk.createproj(r=self.rdcheck.get(),st=self.staticcheckvar.get(),sr=self.srcditcheck.get(),rq=self.reqcheckvar.get(),d=self.ddir,md=self.moduledata)
 
         self.Frame_remprojframe()
+
+    def saveProject(self):
+        self.Frame_remViewprojframe()
+        self.Direct_Backend.Uninstall_Modules( self.removeModules_FromCreatedProj_list)
+        print(self.removeModules_FromCreatedProj_list)
+        self.Direct_Backend.Install_Module( self.installModuleToProject)
+        
+        pass
 if __name__=="__main__":
 
     app = App()
--- backend.py
+++ backend.py
@@ -1,8 +1,46 @@
 import os,json
+import requests
+
 class backendworks:
-    def __init__(self) -> None:
-        pass
+    def __init__(self,CWP) -> None:
+        self.Current_working_project=str(CWP) 
+        with open('data.json', 'r') as file:
+            data = json.load(file)
+        self.CWD_env=data["Project"][self.Current_working_project]["env"]
+        self.CWD_settings=data["Project"][self.Current_working_project]["settings"]
+        self.CWD_path=data["Project"][self.Current_working_project]["path"].replace("/", "\\")
+    def getModules_dict(self):
+        Temp_Modules={"modules":{}}
+        return Temp_Modules
+    def getModule(self,mdlname,version):
+        url = f"https://pypi.org/pypi/{mdlname}/json"
+        response = requests.get(url)
+        if  response.status_code == 200:
+            version=str(version)
+            if version in response.json()["releases"] or version=="":
+                return True
+            else:
+                return "no such version"
+        else:
+            return "no such module"
+    def getmodules_json(self):
+        # with open(self.CWD_settings, "r") as settings_porject:
+        #     data = json.load(settings_porject)
+        # return data
+        modules_versions_dict = {"modules":{}}
+
+        # Read information from req.txt and populate the dictionary
+        with open(f"{self.CWD_path}\\requirements.txt", 'r') as file:
+            for line in file:
+                # Split the line into module name and version
+                module_name, module_version = line.strip().split('==')
+                # Add the module and its version to the dictionary
+                modules_versions_dict["modules"][module_name] = module_version
+
+        # Print the resulting dictionary
+        return modules_versions_dict
     def stratproject(self,readmevalue,staticvalue,srcvalue,reqvalue,dirr,mdldata,*args):
+        cwd=os.getcwd()
         os.chdir(dirr)
         os.system("virtualenv env ")
         os.system(f"{dirr}/env/Scripts/activate")
@@ -28,4 +66,58 @@ class backendworks:
 
         with open("setting.json","w+") as settings:
             json.dump(mdldata, settings, indent=4)
+
+        os.chdir(cwd)
+        with open('data.json', 'r') as file:
+            data = json.load(file)
+        last_directory = os.path.basename(os.path.normpath(dirr))
+        DataTemplate={"language": "Python",
+            "path": dirr,
+            "env": f"{dirr}/env/Scripts/activate.bat",
+            "settings": f"{dirr}/setting.json"}
+       
+        # Step 2: Modify the data
+        data["Project"][last_directory] = DataTemplate
+
+        # Step 3: Write back to the JSON file
+        with open('data.json', 'w') as file:
+            json.dump(data, file, indent=4)
+
+        self.changeproject(last_directory)
+        with open(f"{self.CWD_path}\\requirements.txt", 'w') as req_file:
+            for module, version in self.getmodules_json().items():
+                req_file.write(f"{module}=={version}\n")
+
+    def changeproject(self,CWP):
+        self.Current_working_project=str(CWP) 
+        with open('data.json', 'r') as file:
+            data = json.load(file)
+        self.CWD_env=data["Project"][self.Current_working_project]["env"]
+        self.CWD_settings=data["Project"][self.Current_working_project]["settings"]
+        self.CWD_path=data["Project"][self.Current_working_project]["path"].replace("/", "\\")
+    def Uninstall_Modules(self,Mdl):
+        os.chdir(self.CWD_path)
+        with open("setting.json","r")as file:
+            data=json.load(file)
         
+        for module in Mdl:
+            os.system(f"""{self.CWD_path}/env/Scripts/activate && echo y | pip uninstall {str(module)}""")
+            print(Mdl)
+            del data["modules"][module]
+       
+        with open(f"{self.CWD_path}\\requirements.txt", 'w') as req_file:
+            for module, version in self.getmodules_json().items():
+                req_file.write(f"{module}=={version}\n")
+          
+    def Install_Module(self,modules_To_install):
+        os.chdir(self.CWD_path)
+        os.system(f"{self.CWD_path}/env/Scripts/activate")
+        for mdls in modules_To_install:
+            verison_ofmdl=str(modules_To_install[mdls]).replace("/",".")
+            if verison_ofmdl=="":
+                os.system(f"""{self.CWD_path}/env/Scripts/activate && pip install {str(mdls)}""")
+            else:    
+                os.system(f"""{self.CWD_path}/env/Scripts/activate && pip install {str(mdls)}=={verison_ofmdl}""")
+        with open(f"{self.CWD_path}\\requirements.txt", 'w') as req_file:
+            for module, version in self.getmodules_json().items():
+                req_file.write(f"{module}=={version}\n")
